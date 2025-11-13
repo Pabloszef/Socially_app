1f7ced38d46a6ab01e3cd87c09e95207345a24aa
Pawel <pawel.zaremba013@gmail.com>
2025-11-13 19:55:27 +0100

    Finish Shadcn Setuo & Dark Mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { type Metadata } from 'next'
 import { ClerkProvider,} from '@clerk/nextjs'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
+import {ThemeProvider} from "@/components/ThemeProvider";
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -25,7 +26,14 @@ export default function RootLayout({children,}: Readonly<{
         <ClerkProvider>
             <html lang="en">
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+            >
             {children}
+            </ThemeProvider>
             </body>
             </html>
         </ClerkProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
 import {SignedIn, SignedOut, SignInButton, UserButton} from "@clerk/nextjs";
+import {Button} from "@/components/ui/button";
+import ModeToggle from "@/components/ModeToggle";
 
 
 export default function Home() {
   return (
-    <div>
+    <div className="m-4">
         <SignedOut>
             <SignInButton mode="modal">
-                <button className="bg-red-500">
-                    Sign In
-                </button>
+                <Button>Sign In</Button>
             </SignInButton>
         </SignedOut>
         <SignedIn>
             <UserButton />
         </SignedIn>
+
+        <ModeToggle />
+        <Button>Sign In</Button>
     </div>
   );
 }
