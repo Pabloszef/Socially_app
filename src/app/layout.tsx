fa7e877e307709f3237ea2ac1a7720184d5c5f74
Pawel <pawel.zaremba013@gmail.com>
2025-11-14 17:55:00 +0100

    Finish creating Navbar and Footer components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
-import { type Metadata } from 'next'
-import { ClerkProvider,} from '@clerk/nextjs'
-import { Geist, Geist_Mono } from 'next/font/google'
+import {type Metadata} from 'next'
+import {ClerkProvider,} from '@clerk/nextjs'
+import {Geist, Geist_Mono} from 'next/font/google'
 import './globals.css'
 import {ThemeProvider} from "@/components/ThemeProvider";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -24,7 +26,7 @@ export default function RootLayout({children,}: Readonly<{
 }>) {
     return (
         <ClerkProvider>
-            <html lang="en">
+            <html lang="en" suppressHydrationWarning>
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
             <ThemeProvider
                 attribute="class"
@@ -32,7 +34,24 @@ export default function RootLayout({children,}: Readonly<{
                 enableSystem
                 disableTransitionOnChange
             >
-            {children}
+                <div className="min-w-screen">
+                    <Navbar/>
+
+                    <main className="py-8">
+                        {/*  container to center the content  */}
+                        <div className="max-w-7xl mx-auto px-4">
+                            <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                                <div className="hidden lg:block lg:col-span-3">
+                                    sidebar
+                                </div>
+                                <div className="lg:col-span-9">
+                                    {children}
+                                </div>
+                            </div>
+                        </div>
+                    </main>
+                </div>
+                    <Footer/>
             </ThemeProvider>
             </body>
             </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,8 @@
-import {SignedIn, SignedOut, SignInButton, UserButton} from "@clerk/nextjs";
-import {Button} from "@/components/ui/button";
-import ModeToggle from "@/components/ModeToggle";
-
 
 export default function Home() {
   return (
     <div className="m-4">
-        <SignedOut>
-            <SignInButton mode="modal">
-                <Button>Sign In</Button>
-            </SignInButton>
-        </SignedOut>
-        <SignedIn>
-            <UserButton />
-        </SignedIn>
-
-        <ModeToggle />
-        <Button>Sign In</Button>
+        <h1>HomePage</h1>
     </div>
   );
 }
