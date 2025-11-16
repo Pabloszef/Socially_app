2e0c943606aa51f68b66b499b5a3d2cb86863c7c
Pawel <pawel.zaremba013@gmail.com>
2025-11-16 16:24:08 +0100

    Finish Like & Comment & Delete Posts

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -60,7 +60,7 @@ export async function getUserByClerkId(clerkId: string) {
 
 export async function getDbUserId() {
     const {userId: clerkId} = await auth();
-    if (!clerkId) throw new Error("Unauthorized");
+    if (!clerkId) return null
 
     const user = await getUserByClerkId(clerkId);
 
@@ -72,6 +72,8 @@ export async function getDbUserId() {
 export async function getRandomUsers() {
     try {
         const userId = await getDbUserId()
+
+        if (!userId) return []
 
         // get 3 random users exclude ourselves & users that we already follow
         const randomUsers = await prisma.user.findMany({
@@ -114,6 +116,8 @@ export async function getRandomUsers() {
 export const toggleFollow = async (targetUserId: string) => {
     try {
         const userId = await getDbUserId()
+
+        if (!userId) return
 
         if (userId === targetUserId) throw new Error("You cannot follow yourself");
 
