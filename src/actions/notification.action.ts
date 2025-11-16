cf5ee525517a551a552a9646d2a87f68fda6fa56
Pawel <pawel.zaremba013@gmail.com>
2025-11-16 19:20:06 +0100

    Finish Notifications Page

---
+++ src/actions/notification.action.ts
@@ -0,0 +1,89 @@
+"use server"
+
+import {getDbUserId} from "@/actions/user.action";
+import prisma from "@/lib/prisma";
+import {revalidatePath} from "next/cache";
+
+export async function getNotifications() {
+    try {
+        const userId = await getDbUserId();
+        if (!userId) return [];
+
+        const notifications = await prisma.notification.findMany({
+            where: {
+                userId,
+            },
+            include: {
+                creator: {
+                    select: {
+                        id: true,
+                        name: true,
+                        username: true,
+                        image: true,
+                    },
+                },
+                post: {
+                    select: {
+                        id: true,
+                        content: true,
+                        image: true,
+                    },
+                },
+                comment: {
+                    select: {
+                        id: true,
+                        content: true,
+                        createdAt: true,
+                    },
+                },
+            },
+            orderBy: {
+                createdAt: "desc",
+            },
+        });
+
+        revalidatePath("/notifications")
+
+        return notifications;
+    } catch (error) {
+        console.error("Error fetching notifications:", error);
+        throw new Error("Failed to fetch notifications");
+    }
+}
+
+export async function markNotificationAsRead(notificationIds: string[]) {
+    try {
+        await prisma.notification.updateMany({
+            where: {
+                id: {
+                    in: notificationIds
+                },
+            },
+            data: {
+                read: true,
+            }
+        });
+
+        return {success: true}
+    } catch (error) {
+        console.error("Error marking notifications as read:", error);
+        return {success: false}
+    }
+}
+
+export async function deleteNotification(notificationId: string) {
+    try {
+        await prisma.notification.delete({
+            where: {
+                id: notificationId,
+            },
+        });
+
+        revalidatePath("/notifications");
+
+        return {success: true}
+    } catch (error) {
+        console.error("Error deleting notification:", error);
+        return {success: false}
+    }
+}
