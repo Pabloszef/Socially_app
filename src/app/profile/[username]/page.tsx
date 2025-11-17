36bdb2e6c4bba3406d50f50a32752a6bfd7c7c14
Pawel <pawel.zaremba013@gmail.com>
2025-11-17 20:50:05 +0100

    Ready to deploy

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -9,7 +9,9 @@ import ProfilePageClient from "./ProfilePageClient";
 
 export async function generateMetadata({ params }: { params: { username: string } }) {
   const user = await getProfileByUsername(params.username);
-  if (!user) return;
+  if (!user) { // @ts-ignore
+      return;
+  }
 
   return {
     title: `${user.name ?? user.username}`,
